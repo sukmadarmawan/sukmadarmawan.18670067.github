e33197c52026b89b29ab70b3cf7bb338b4eda379
sukmadarmawan <75522760+sukmadarmawan@users.noreply.github.com>
2021-07-07 08:11:27 +0700

    upload uas

---
+++ webgis/layers/Nelayan_3.js
@@ -0,0 +1,18 @@
+var json_Nelayan_3 = {
+"type": "FeatureCollection",
+"name": "Nelayan_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Kecamatan": "Kedung", "Nelayan": 1974.0 }, "geometry": { "type": "Point", "coordinates": [ 110.657865186621166, -6.687742036665361 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Kecamatan": "Pecangaan", "Nelayan": 537.0 }, "geometry": { "type": "Point", "coordinates": [ 110.707689640702796, -6.70767181829801 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Kecamatan": "Welahan", "Nelayan": 557.0 }, "geometry": { "type": "Point", "coordinates": [ 110.71493683402376, -6.77289655818668 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Kecamatan": "Mayong", "Nelayan": 626.0 }, "geometry": { "type": "Point", "coordinates": [ 110.777443876417067, -6.70767181829801 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Kecamatan": "Nalumsari", "Nelayan": 199.0 }, "geometry": { "type": "Point", "coordinates": [ 110.813679843021887, -6.726695700765539 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Kecamatan": "Batealir", "Nelayan": 192.0 }, "geometry": { "type": "Point", "coordinates": [ 110.772914380591459, -6.633388086758136 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Kecamatan": "Jepara", "Nelayan": 324.0 }, "geometry": { "type": "Point", "coordinates": [ 110.673265472428213, -6.587187229336995 ] } },
+{ "type": "Feature", "properties": { "id": "8", "Kecamatan": "Tahunan", "Nelayan": 207.0 }, "geometry": { "type": "Point", "coordinates": [ 110.690477556565497, -6.631576288427895 ] } },
+{ "type": "Feature", "properties": { "id": "9", "Kecamatan": "Mlonggo", "Nelayan": 228.0 }, "geometry": { "type": "Point", "coordinates": [ 110.73577251482152, -6.569069246034586 ] } },
+{ "type": "Feature", "properties": { "id": "10", "Kecamatan": "Bangsri", "Nelayan": 215.0 }, "geometry": { "type": "Point", "coordinates": [ 110.804620851370672, -6.514715296127362 ] } },
+{ "type": "Feature", "properties": { "id": "11", "Kecamatan": "Keling", "Nelayan": 149.0 }, "geometry": { "type": "Point", "coordinates": [ 110.891587171222241, -6.480291127852785 ] } }
+]
+}
